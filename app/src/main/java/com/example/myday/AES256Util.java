43b47efa728fea770d4c355f79ddf0502a313d25
dululy <kangjh0819@gmail.com>
2020-06-23 20:55:08 +0900

    최종

---
+++ app/src/main/java/com/example/myday/AES256Util.java
@@ -0,0 +1,70 @@
+package com.example.myday;
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.apache.commons.codec.binary.Base64;
+
+public class AES256Util {
+    private String iv;
+    private Key keySpec;
+    public static final String KEY = "aes256-test-key!!";
+
+    public AES256Util(String key)throws UnsupportedEncodingException {
+        this.iv = key.substring(0,16);
+
+        byte[] keyBytes = new byte[16];
+        byte[] b = key.getBytes("UTF-8");
+        int length = b.length;
+
+        if(length > keyBytes.length){
+            length = keyBytes.length;
+        }
+
+        System.arraycopy(b,0,keyBytes,0,length);
+        SecretKeySpec keySpec =  new SecretKeySpec(keyBytes, "AES");
+
+        this.keySpec = keySpec;
+    }
+
+    public String aesEncode(String str) throws java.io.UnsupportedEncodingException,
+            NoSuchAlgorithmException,
+            NoSuchPaddingException,
+            InvalidKeyException,
+            InvalidAlgorithmParameterException,
+            IllegalBlockSizeException,
+            BadPaddingException{
+        Cipher c = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        c.init(Cipher.ENCRYPT_MODE, keySpec, new IvParameterSpec(iv.getBytes()));
+
+        byte[] encrypted = c.doFinal(str.getBytes("UTF-8"));
+        String enStr = new String(Base64.encodeBase64(encrypted));
+
+        return enStr;
+    }
+
+    public String aesDecode(String str) throws java.io.UnsupportedEncodingException,
+            NoSuchAlgorithmException,
+            NoSuchPaddingException,
+            InvalidKeyException,
+            InvalidAlgorithmParameterException,
+            IllegalBlockSizeException,
+            BadPaddingException {
+
+        Cipher c = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        c.init(Cipher.DECRYPT_MODE, keySpec, new IvParameterSpec(iv.getBytes()));
+
+        byte[] byteStr = Base64.decodeBase64(str.getBytes());
+
+        return new String(c.doFinal(byteStr), "UTF-8");
+    }
+}
---
+++ app/src/main/java/com/example/myday/MainSetting.java
@@ -0,0 +1,120 @@
+package com.example.myday;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+import android.view.View;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.myday.applock.App;
+import com.example.myday.applock.HomePage;
+import com.google.firebase.iid.FirebaseInstanceId;
+
+
+public class MainSetting extends AppCompatActivity {
+
+    private Button setting_notification;
+    private Button setting_password;
+    private ImageButton back_btn;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_setting);
+
+        setting_notification = findViewById(R.id.setting_notification);
+        setting_password = findViewById(R.id.setting_password);
+
+
+        //뒤로가기 버튼 동작
+        back_btn = findViewById(R.id.back_btn);
+        back_btn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent main = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(main);
+                finish();
+            }
+        });
+
+        setting_notification.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+//                Intent intent = new Intent(getApplicationContext(), NotiSettingsActivity.class);
+            }
+        });
+
+        setting_password.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), HomePage.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+    }
+
+    //뒤로가기시
+    @Override
+    public void onBackPressed() {
+        Intent main = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(main);
+        finish();
+    }
+
+}
+//public class MainSetting extends PreferenceActivity implements Preference.OnPreferenceClickListener {
+//
+//    Preference pPassword;
+//    Preference pNotification;
+//    @Override
+//    public void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        addPreferencesFromResource(R.xml.setting_preference);
+//
+//        //암호설정메뉴, 알림설정메뉴 데려오기
+//        pNotification = findPreference("notification_setting");
+//        pPassword= findPreference("password_setting");
+//
+//
+//
+//        pNotification.setOnPreferenceClickListener(this);
+//        pPassword.setOnPreferenceClickListener(this);
+//    }
+//
+////누르면 페이지 넘어가게하는 부분
+//    public boolean onPreferenceClick(Preference preference) {
+//
+//        if (preference.hasKey()) {
+//
+//            String preferenceKey = preference.getKey();
+//
+//            if ("notification_setting".equals(preferenceKey)) {
+////                Toast.makeText(this, "알림설정", Toast.LENGTH_SHORT).show();
+////                Intent intent = new Intent(getApplicationContext(), NotiSettingsActivity.class);
+////                startActivity(intent);
+//            } else if ("password_setting".equals(preferenceKey)) {
+////                Toast.makeText(this, "암호설정", Toast.LENGTH_SHORT).show();
+//                Intent intent = new Intent(getApplicationContext(), HomePage.class);
+//                startActivity(intent);
+//            }
+//        }
+//
+//        return false;
+//
+//    }
+//
+//}
+
+
